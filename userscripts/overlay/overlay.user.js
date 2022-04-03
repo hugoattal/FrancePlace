782761b9c0ece3b709ac5e1679415193947de453
Hugo <hugoattal@hotmail.fr>
2022-04-03 17:39:50 +0200

    Fix overlay link

--- userscripts/overlay/overlay.user.js
+++ userscripts/overlay/overlay.user.js
@@ -2,7 +2,7 @@
 // @name         r/FrancePlace Overlay
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Pour la France.
+// @description  Overlay pour r/place
 // @author       Herobrine
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://place.luna-park.fr/favicon.png
@@ -12,7 +12,7 @@
 if (window.top !== window.self) {
     window.addEventListener("load", () => {
         const overlay = document.createElement("img");
-        overlay.src = "https://github.com/hugoattal/FrancePlace/tree/main/userscripts/overlay/output.png";
+        overlay.src = "https://raw.githubusercontent.com/hugoattal/FrancePlace/main/userscripts/overlay/output.png";
         overlay.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
 
         document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(overlay);
