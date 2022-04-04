bca5d0646a012ef4c3c41938f69db641b544d8f3
Hugo <hugoattal@hotmail.fr>
2022-04-04 16:52:46 +0200

    Update map

--- userscripts/overlay/sanitizeMap.ts
+++ userscripts/overlay/sanitizeMap.ts
@@ -1,23 +1,42 @@
 import sharp from "sharp";
 
 const RGB_CODE_DICTIONARY = [
-    [255, 69, 0],
-    [255, 168, 0],
-    [255, 214, 53],
-    [0, 163, 96],
-    [126, 237, 86],
-    [36, 80, 164],
-    [54, 144, 234],
-    [81, 233, 244],
-    [129, 30, 159],
-    [180, 74, 192],
-    [255, 153, 170],
-    [156, 105, 38],
-    [0, 0, 0],
-    [137, 141, 144],
-    [212, 215, 217],
-    [255, 255, 255]
+    [109,0,26],
+    [190,0,57],
+    [255,69,0],
+    [255,168,0],
+    [255,214,53],
+    [255,248,184],
+    [0,163,104],
+    [0,204,120],
+    [126,237,86],
+    [0,117,111],
+    [0,158,170],
+    [0,204,192],
+    [36,80,164],
+    [54,144,234],
+    [81,233,244],
+    [73,58,193],
+    [106,92,255],
+    [148,179,255],
+    [129,30,159],
+    [180,74,192],
+    [228,171,255],
+    [222,16,127],
+    [255,56,129],
+    [255,153,170],
+    [109,72,47],
+    [156,105,38],
+    [255,180,112],
+    [0,0,0],
+    [81,82,82],
+    [137,141,144],
+    [212,215,217],
+    [255,255,255]
 ];
+
+
+
 
 
 
@@ -54,11 +73,20 @@ type TColor = {
             const color = getPixel(inSize, inputMap.data, { x, y });
 
             if (color.a === 255) {
-                if (RGB_CODE_DICTIONARY.find((value) => {
-                    return value[0] === color.r && value[1] === color.g && value[2] === color.b;
-                })) {
-                    setPixel(inSize, outputMap.data, { x, y }, color);
+                RGB_CODE_DICTIONARY.sort((value1, value2) => {
+                    return distColor(value1) - distColor(value2);
+
+                    function distColor(value: Array<number>): number {
+                        return Math.pow(value[0] - color.r, 2) + Math.pow(value[1] - color.g, 2) + Math.pow(value[2] - color.b, 2);
+                    }
+                });
+
+                const sColor = RGB_CODE_DICTIONARY[0];
+
+                if (sColor) {
+                    setPixel(inSize, outputMap.data, { x, y }, { a: 255, b: sColor[2], g: sColor[1], r: sColor[0] });
                 }
+
             }
         }
     }
